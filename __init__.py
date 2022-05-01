04bf8082916524251cd2b1831816464a5a62d778
MubaraqSani <mubaraq.sani@e4email.net>
2022-05-01 21:50:19 +0100

    Initialized the app blueprint

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+# init db so that it can be used later
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://db.sqlite'
+    
+    db.init_app(app)
+    
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    
+    # blueprint for non-auth part of the app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    return app

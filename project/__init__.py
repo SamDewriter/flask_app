ce4005b8d904104ef1b91c7c250bf5de078d42d3
MubaraqSani <mubaraq.sani@e4email.net>
2022-05-02 21:14:07 +0100

    Setting up a database

--- project/__init__.py
+++ project/__init__.py
@@ -8,7 +8,7 @@ def create_app():
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = ''
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     
     db.init_app(app)
     

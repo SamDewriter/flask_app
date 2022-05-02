55bc99499797901b522ce37a9d759415f44adfff
MubaraqSani <mubaraq.sani@e4email.net>
2022-05-02 21:31:00 +0100

    Setting up user authentication

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,7 @@
 from crypt import methods
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
